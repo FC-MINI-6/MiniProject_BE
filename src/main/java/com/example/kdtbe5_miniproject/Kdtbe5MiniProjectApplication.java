334c46012e3082df2eec3dc924024eaede9cb374
JOOWON <wer07032@naver.com>
2023-07-24 15:32:30 +0900

    init

---
+++ src/main/java/com/example/kdtbe5_miniproject/Kdtbe5MiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.kdtbe5_miniproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kdtbe5MiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kdtbe5MiniProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kdtbe5_miniproject/Kdtbe5MiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kdtbe5_miniproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kdtbe5MiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
