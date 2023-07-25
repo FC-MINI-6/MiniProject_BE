62e7ad64d4b3eefe46570bfd469d931f54f998f8
JOOWON <wer07032@naver.com>
2023-07-25 16:41:17 +0900

    feat:#1 User Service

--- src/main/java/com/example/kdtbe5_miniproject/user/UserService.java
+++ src/main/java/com/example/kdtbe5_miniproject/user/UserService.java
@@ -1,7 +1,21 @@
 package com.example.kdtbe5_miniproject.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public UserResponse.JoinDTO joinUser(UserRequest.JoinDTO joinDTO) {
+        joinDTO.setPassword(passwordEncoder.encode(joinDTO.getPassword()));
+        User userPS = userRepository.save(joinDTO.toEntity());
+        return new UserResponse.JoinDTO(userPS);
+    }
 }
