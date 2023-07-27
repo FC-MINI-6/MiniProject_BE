04b6675104732e7109aa8f3af35c9beb812cea19
JOOWON <wer07032@naver.com>
2023-07-27 13:29:10 +0900

    feat:#1 회원가입 시 사용자 정보 암호화 적용, 주석 처리
    암호화를 먼저 적용하면 헷갈릴 것 같아 주석처리 해뒀습니다

--- src/main/java/com/example/kdtbe5_miniproject/user/UserService.java
+++ src/main/java/com/example/kdtbe5_miniproject/user/UserService.java
@@ -1,5 +1,6 @@
 package com.example.kdtbe5_miniproject.user;
 
+import com.example.kdtbe5_miniproject._core.util.EncryptUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,12 @@ public class UserService {
     @Transactional
     public UserResponse.JoinDTO joinUser(UserRequest.JoinDTO joinDTO) {
         joinDTO.setPassword(passwordEncoder.encode(joinDTO.getPassword()));
+
+        //사용자 정보 암호화 - 기능 구현 시 헷갈릴 것 같아서 주석 처리
+//        joinDTO.setUsername(EncryptUtils.encrypt(joinDTO.getUsername()));
+//        joinDTO.setPhoneNumber(EncryptUtils.encrypt(joinDTO.getPhoneNumber()));
+//        joinDTO.setEmail(EncryptUtils.encrypt(joinDTO.getEmail()));
+
         User userPS = userRepository.save(joinDTO.toEntity());
         return new UserResponse.JoinDTO(userPS);
     }
