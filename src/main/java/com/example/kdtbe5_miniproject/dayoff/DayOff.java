547d27446dd3971fbeb0c8a3e572bf07dbea4384
Park SeongWook <116128609+gosuuk@users.noreply.github.com>
2023-07-27 21:28:00 +0900

    model 생성

---
+++ src/main/java/com/example/kdtbe5_miniproject/dayoff/DayOff.java
@@ -0,0 +1,31 @@
+package com.example.kdtbe5_miniproject.dayoff;
+
+import com.example.kdtbe5_miniproject.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Table(name = "dayoff_tb")
+public class DayOff {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String reason;
+    private String type;    // 연차, 오전반차, 오후반차
+    private String status;  // 대기, 승인, 반려
+}
---
+++ src/main/java/com/example/kdtbe5_miniproject/duty/Duty.java
@@ -0,0 +1,30 @@
+package com.example.kdtbe5_miniproject.duty;
+
+import com.example.kdtbe5_miniproject.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Table(name = "duty_tb")
+public class Duty {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String content;
+    private String status;  // 대기, 승인, 반려
+}
