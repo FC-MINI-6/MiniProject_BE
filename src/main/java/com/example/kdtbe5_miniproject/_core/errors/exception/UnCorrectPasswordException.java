ebca4e3248598f909f9c3be4cb52e57643974c86
JOOWON <wer07032@naver.com>
2023-07-31 21:32:35 +0900

    feat: #3 비밀번호가 맞지 않을 때 발생하는 예외 생성

---
+++ src/main/java/com/example/kdtbe5_miniproject/_core/errors/exception/UnCorrectPasswordException.java
@@ -0,0 +1,8 @@
+package com.example.kdtbe5_miniproject._core.errors.exception;
+
+public class UnCorrectPasswordException extends RuntimeException{
+
+    public UnCorrectPasswordException(String message) {
+        super(message);
+    }
+}
