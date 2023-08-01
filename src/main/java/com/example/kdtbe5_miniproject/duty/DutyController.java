1de167b21e5330fc8524049c022234179802c115
HanHyeJi <hjhan.0621@gmail.com>
2023-08-02 01:21:10 +0900

    refacor:#4 DutyController > 메서드 주석(설명)추가

--- src/main/java/com/example/kdtbe5_miniproject/duty/DutyController.java
+++ src/main/java/com/example/kdtbe5_miniproject/duty/DutyController.java
@@ -11,12 +11,14 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@RequestMapping("/mypage")
 @RequiredArgsConstructor
 @RestController
 public class DutyController {
 
     private final DutyService dutyService;
 
+    // 당직 신청
     @PostMapping("/duty/register")
     public ResponseEntity<?> createDuty(@RequestBody @Valid DutyRequest.DutyDTO dutyDTO, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         User user = customUserDetails.getUser();
@@ -24,7 +26,7 @@ public class DutyController {
         return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
     }
 
-
+    // 내 당직 리스트
     @GetMapping("/duty/applied")
     public ResponseEntity<?> getAppliedDuty(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
         User user = customUserDetails.getUser();
@@ -32,6 +34,7 @@ public class DutyController {
         return ResponseEntity.ok().body(ApiUtils.success(appliedDuties));
     }
 
+    // 당직 신청 취소
     @DeleteMapping("/duty/{dutyId}")
     public ResponseEntity<?> deleteDuty(@PathVariable Long dutyId, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         User user = customUserDetails.getUser();
