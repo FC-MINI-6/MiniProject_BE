7cd576656e18c0bdd4d2022500f4ce95dcd16652
HanHyeJi <hjhan.0621@gmail.com>
2023-08-02 01:15:44 +0900

    refacor:#4 DayOffService, DayOffController > 메서드 주석(설명) 추가

--- src/main/java/com/example/kdtbe5_miniproject/dayoff/DayOffController.java
+++ src/main/java/com/example/kdtbe5_miniproject/dayoff/DayOffController.java
@@ -11,11 +11,13 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@RequestMapping("/mypage")
 @RequiredArgsConstructor
 @RestController
 public class DayOffController {
     private final DayOffService dayOffService;
 
+    // 연차 신청
     @PostMapping("/dayoff/register")
     public ResponseEntity<?> registerDayOff(@RequestBody @Valid DayOffRequest.RegisterDTO registerDTO, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         User user = customUserDetails.getUser();
@@ -30,14 +32,16 @@ public class DayOffController {
         return ResponseEntity.ok().body(ApiUtils.success(myDayOffDTO));
     }
 
-    @GetMapping("/dayoff/applied")
+    // 내 연차 리스트
+    @GetMapping("/dayoffList")
     public ResponseEntity<?> myAppliedDayOffs(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
         User user = customUserDetails.getUser();
         DayOffResponse.AppliedDayOffDTO[] appliedDayOffDTOs = dayOffService.myAppliedDayOffs(user.getId());
         return ResponseEntity.ok().body(ApiUtils.success(appliedDayOffDTOs));
     }
 
-    @DeleteMapping("/dayoff/{dayoffId}")
+    // 연차 신청 취소
+    @DeleteMapping("dayoff/{dayoffId}")
     public ResponseEntity<?> cancelDayOff(@PathVariable Long dayoffId, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         User user = customUserDetails.getUser();
         dayOffService.cancelDayOff(dayoffId, user.getId());
