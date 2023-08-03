05cbcab368c23537a4289c0236d8848fd1c15506
Park SeongWook <116128609+gosuuk@users.noreply.github.com>
2023-08-03 14:02:32 +0900

    #17 월별 스케줄, 전체 사용자 조회

--- src/main/java/com/example/kdtbe5_miniproject/user/UserRepository.java
+++ src/main/java/com/example/kdtbe5_miniproject/user/UserRepository.java
@@ -1,5 +1,4 @@
 package com.example.kdtbe5_miniproject.user;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -7,15 +6,16 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-
     Optional<User> findByEmail(@Param("email") String email);
-
     @Modifying
     @Query(value = "UPDATE User SET password = :newPassword WHERE id = :id")
     void updateById(@Param("newPassword") String newPassword, @Param("id") Long id);
+
+    @Override
+    List<User> findAll();
 }
